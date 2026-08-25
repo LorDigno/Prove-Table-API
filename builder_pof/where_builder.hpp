ea85dff0220ae12f1f45aef3a730e6245860819a
Duccio Lorenzoni <d.lorenzoni4@gmail.com>
2026-08-25 12:07:56 +0200

    removed where keyBy

--- builder_pof/where_builder.hpp
+++ builder_pof/where_builder.hpp
@@ -23,15 +23,12 @@ public:
 };
 
 //vero e proprio builder che gestisce i parametri e istanzia la Filter
-template <typename InputT, typename KeyT = std::string>
+template <typename InputT>
 class Where_Builder {
 private:
     std::function<bool(const InputT&)> filt_func;
     std::string op_name = "Where_Operator";
     size_t parallelism = 1;
-
-    bool keyed = false;
-    std::function<KeyT(const InputT&)> key_func;
 
 public:
     //associa la lambda
@@ -49,24 +46,12 @@ public:
         this->parallelism = par;
         return *this;
     }
-
-    //stabilisce la funzione d'estrazione della chiave
-    Where_Builder& withKeyBy(std::function<KeyT(const InputT&)> k_func) {
-        this->key_func = k_func;
-        this->keyed = true;
-        return *this;
-    }
-
-    //istanzia, con tutti i parametri dati, sul Select_Functor e rende la Map nativa 
+ 
     auto build() {
-        auto effective_key_func = keyed ? key_func : [](const InputT&) { return KeyT{}; };
-        size_t effective_parallelism = keyed ? parallelism : 1;
-
         Where_Functor<InputT> functor(filt_func);
         return wf::Filter_Builder(functor)
             .withName(op_name)
-            .withParallelism(effective_parallelism)
-            .withKeyBy(effective_key_func)
+            .withParallelism(parallelism)
             .build();
     }
 };
